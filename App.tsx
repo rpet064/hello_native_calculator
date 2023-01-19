5c0ab6a8ee7779622faa19b5c1098ea4a58f4e1f
rpet064 <93119631+rpet064@users.noreply.github.com>
2023-01-20 10:50:17 +1300

    placement of screen & buttons in progress

--- App.tsx
+++ App.tsx
@@ -1,20 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// React Native Counter Example using Hooks!
 
-export default function App() {
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet, Button, StatusBar,} from 'react-native';
+// import CalculatorButtons from './components/calculatorButtons.tsx'
+
+const symbolsArray = [
+  "AC", "C", "√", "÷",
+  "7", "8", "9", "×",
+  "4", "5", "6", "-",
+  "1", "2", "3", "+",
+  "0", "+/-", ".", "="
+  ]
+
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.calculatorScreen}>
+        <Text>This is the screen</Text>
+      </View>
+      <View style={styles.calculatorKeypad}>
+        {symbolsArray.map((symbol, index) => {
+          return (
+            <TouchableOpacity onPress={() => console.log({symbol})} key={index} style={styles.calcBtn}>
+              <Text>{symbol}</Text>
+            </TouchableOpacity>
+          )
+        })
+        }
+      </View>
+      
     </View>
   );
-}
+};
 
+// React Native Styles
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flex: 2,
+    flexDirection: 'column',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
   },
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'red',
+    padding: 10,
+    marginBottom: 10,
+    borderWidth:2,
+  },
+  calculatorScreen:{
+    minHeight: 169.6,
+    margin: 'auto',
+  },
+  calculatorKeypad:{
+    minHeight: '100%',
+    display: 'flex',
+    flexWrap: 'wrap',
+    padding: '0 0.1%',
+  },
+  calcBtn:{
+    flex: 4,
+    backgroundColor: '#FFFAE7',
+    color: '#181D31',
+    minHeight: 68,
+    fontSize: 52,
+    border: '#181D31 1px solid',
+  }
 });
+
+export default App;
---
+++ components/CalculatorButtons.tsx
@@ -0,0 +1,36 @@
+// import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
+
+// type CalculatorProps = {
+//     count: Number;
+//   };
+  
+
+// const CalculatorButtons = (props: CalculatorProps) => {
+//     <View style={styles.container}>
+//       <Text>You clicked</Text>
+//         <TouchableOpacity style={styles.button} onPress={() => console.log("Clicked")}>
+//               <Text style={styles.text}>Login</Text>
+//         </TouchableOpacity>
+//     </View>
+//   }
+
+//   // React Native Styles
+// const styles = StyleSheet.create({
+//     container: {
+//       flex: 2,
+//       flexDirection: 'column',
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//       padding: 20,
+//       backgroundColor: 'red',
+//     },
+//     button: {
+//       alignItems: 'center',
+//       backgroundColor: 'red',
+//       padding: 10,
+//       marginBottom: 10,
+//       borderWidth:2,
+//     },
+//   });
+
+// export default CalculatorButtons;
